d5b73af44a8274cf50f78ad459df754cdab59f35
nogueiraac <240700ac@gmail.com>
2021-09-29 20:59:34 -0300

    Adding Exeception for create Key.

--- src/key/key.service.ts
+++ src/key/key.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, NotAcceptableException } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException, NotAcceptableException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Key } from './entity/key';
@@ -17,23 +17,32 @@ export class KeyService {
     const qtdKeys = key.length
 
     if (user) {
-       
-      const chavesExistentes = await this.userRepository.findOne(id, {relations:['keys']})
-
-      const verifyQtdKeys = chavesExistentes.keys.map((a) => a)
+      const existingKeys = await this.userRepository.findOne(id, {relations:['keys']})
+      const verifyQtdKeys = existingKeys.keys.map((a) => a)
 
       if ( qtdKeys <= 3 ) {
         if(verifyQtdKeys.length + qtdKeys <= 3) {
-          const keys = key.map((value) => this.keyRepository.create({value, user}))
-          
-          return this.keyRepository.save(keys)
+          if(verifyQtdKeys.length == 0){
+            const keys = key.map((value) => this.keyRepository.create({value, user}))
+            return this.keyRepository.save(keys)
+          } else {
+            for(var i = 0; i < qtdKeys; i++){
+              for( var j = 0; j < verifyQtdKeys.length; j++){
+                if(verifyQtdKeys[j].value === key[i]){
+                  throw new ConflictException("Chaves iguais")
+                }
+              }
+            }
+            const keys = key.map((value) => this.keyRepository.create({value, user}))
+            return this.keyRepository.save(keys)
+          }
         } 
         if(verifyQtdKeys.length + qtdKeys > 3) {
-          throw new NotAcceptableException("Quantidade de tags excede o valor máximo de 3 chaves por usuário")
+          throw new NotAcceptableException("Quantidade de chaves excede o valor máximo de 3 chaves por usuário")
         }  
       } 
       if(qtdKeys > 3) {
-        throw new NotAcceptableException("Quantidade de tags excede o valor máximo de 3 chaves por usuário")
+        throw new NotAcceptableException("Quantidade de chaves excede o valor máximo de 3 chaves por usuário")
       }
     }else {
       throw new NotFoundException(`Usuário não encontrado`);
